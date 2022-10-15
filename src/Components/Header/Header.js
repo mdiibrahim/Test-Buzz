9d6a5a897e2ad9f50579ee009f8255c3fd74dce0
mdiibrahim <mdiibrahim549@gmail.com>
2022-10-15 14:42:33 +0600

    home part are responsive done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,44 @@
 import './Header.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
 const Header = () => {
+    const routes = [
+        {
+            id: 1, name: 'Home', path: '/'
+        },
+        {
+            id: 2, name: 'Blog', path: '/blog'
+        },
+        {
+            id: 3, name: 'Anlysis', path: '/analysis'
+        },
+        {
+            id: 4, name: 'About', path: '/about'
+        }
+    ]
+    
+    
     return (
-        <nav className='bg-primary p-4 text-center d-flex justify-content-evenly sticky-top shadow p-3 mb-5 bg-body rounded '>
-            <Link className='text-dark text-decoration-none' to='/'>Home</Link>
-            <Link className='text-dark text-decoration-none' to='/blog'>Blog</Link>
-            <Link className='text-dark text-decoration-none' to='/analysis'>Analysis</Link>
-            <Link className='text-dark text-decoration-none' to='/about'>About</Link>
+        <nav>
+            <nav className='d-flex justify-content-center shadow p-3 mb-5 bg-body rounded'>
+                {
+                    routes.map(route => {
+                        return (
+                            
+                            <Link
+                                key={route.id}
+                                to={route.path}
+                                className='text-decoration-none mx-auto'
+                                
+                            >
+                            {route.name}</Link>
+                        )
+                    })
+                }
+            </nav>
+
         </nav>
     );
 };
--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -5,11 +5,11 @@ import './Intro.css'
 function Intro() {
     return (
         <div className='bg-dark rounded-2'>
-            <section className='my-5 d-flex align-items-center'>
+            <section className='my-5 d-flex align-items-lg-center justify-content-evenly align-items-end'>
                 <div className='me-4'>
-                    <img className='img-fluid rounded-3' src="header-background 2.jpg" alt="" />
+                    <img className='img-fluid  rounded-3' src="header-background 2.jpg" alt="" />
                 </div>
-                <div className=' text-success'>
+                <div className='text-success intro-text'>
                     <h3>Hey, Test Buzz!!!</h3>
                     <p>"Knowledge becomes power only when we put it into some use". So, let's honour our knowledge by answering some simple GK questions.........</p>
                 </div>
